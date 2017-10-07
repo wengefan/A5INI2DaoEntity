5586cc09f1c6af7973a1e29c133b176a3f671ec6
wengefan <fwg1126@gmail.com>
2017-10-07 16:45:03 +0900

    created

---
+++ A5INI2DaoEntityByT4Template/A5INI2DaoEntityByT4Template/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A5INI2DaoEntityByT4Template
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
